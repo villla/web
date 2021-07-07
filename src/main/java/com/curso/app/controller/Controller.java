a9c9fccdd09179547902c42d682d16267bb06512
villla <villasante.carlos@outlook.com>
2021-07-07 12:39:17 +0200

    Added controller

---
+++ src/main/java/com/curso/app/controller/Controller.java
@@ -0,0 +1,52 @@
+package com.curso.app.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// doGet(request, response);
+		
+		// Recoger el parametro del formulario
+		String name = request.getParameter("name");
+		name = "Hola " + name;
+		
+		// Cargar parametros en la request
+		request.setAttribute("name", name);
+		
+		// Redirigir a la vista
+		RequestDispatcher rd = request.getRequestDispatcher("result.jsp");
+		rd.forward(request, response); // Carga el recurso en el navegador
+	}
+
+}
